7acc02e3ecc28bf151e8fde0cdcc97133d08b1d9
Gustavo Farfan <farfancoraje15@gmail.com>
2022-06-01 01:20:17 -0500

    user attendance details

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -50,4 +52,31 @@ class User extends Authenticatable
             set : fn($value) => Hash::make($value)
         );
     }
+    public function attendances(){
+        return $this->hasMany(Attendance::class);
+    }
+    public function assistances(Carbon $date){
+        $second_date = $date->format('Y-m-d');
+        $first_date = $date->subMonth()->format('Y-m-d');
+        return $this->attendances()->whereBetween('date',[$first_date,$second_date]);
+    }
+    public function tardiness(Carbon $date){
+        $range = $this->assistances($date);
+        return $range->where('checkin_time','>','09:15');
+    }
+    public function absences(Carbon $date){
+        $second_date = $date;
+        $first_date = $date->copy()->subMonth();
+        $total_days = 0;
+        $days = CarbonPeriod::create($first_date, $second_date)->toArray();
+        
+        foreach($days as $day){
+            if($day->format('N') !=7){
+                $total_days++;
+            }
+        }
+        $result = $total_days - $this->assistances($date)->count();
+        return $result;
+    }
+
 }
